dd28a97753cb4280b5ebe495c93b1390125ce91a
Kevin Chi <kevinchi118@gmail.com>
2018-07-22 01:23:00 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.1]
     def change
-        create_table :actors
+        create_table :actors do |t|
+            t.string :full_name
+        end 
     end
 
 end
