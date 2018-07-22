caf21fbf47e6a377708325ecb71bcf10a967c824
Kevin Chi <kevinchi118@gmail.com>
2018-07-22 01:24:40 +0000

    automatically backed up by learn

--- db/migrate/012_add_networkid_to_characters.rb
+++ db/migrate/012_add_networkid_to_characters.rb
@@ -1,5 +1,5 @@
-class AddNetworkidToCharacters < ActiveRecord::Migration[5.1]
+class AddNetworkidToCharacters < ActiveRecord::Migration[4.2]
     def change
         add_column :characters, :network_id, :integer
     end
-end 
+end
